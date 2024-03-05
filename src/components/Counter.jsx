47fbdb7b55b3b55c5a73b9fb3a73567f0d081bb2
Wai Phyo Aung <waefioong@gmail.com>
2024-03-05 18:28:36 +0630

    Redux Tuto

---
+++ src/components/Counter.jsx
@@ -0,0 +1,37 @@
+import {  useDispatch, useSelector } from "react-redux";
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const isShow = useSelector((state)=> state.isShow)
+  const increaseHandler = () => {
+    dispatch({type : "increase"})
+  };
+  const decreaseHandler = () => {
+    dispatch({type : 'decrease'})
+  };
+
+  const increaseBy5Handler = ()=>{
+    dispatch({type : "increaseBy5", amount : 10})
+  }
+
+  const toggleHandler = ()=>{
+    dispatch({type : "isShow"})
+  }
+  return (
+    <section>
+      <h3>Redux Counter</h3>
+      {
+        isShow && <h1>{counter}</h1>
+      }
+      <hr />
+      <div>
+        <button onClick={increaseHandler}>Increase</button>
+        <button onClick={increaseBy5Handler}>IncreaseBy5</button>
+        <button onClick={decreaseHandler}>Decrease</button>
+        <button onClick={toggleHandler}>Toggle</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
